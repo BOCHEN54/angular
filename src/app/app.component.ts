775f159a06e0c2708c21d4a77b6c3f141aba8146
bochen <Kamil.orchowski2@gmail.com>
2024-11-12 00:12:48 +0100

    tak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'formularz';
+  Imie_Nazwisko = '';
+  email = 'prosze podac email';
+  produkt = '';
+  ilosc = 0;
+  wiadomosc = 'Jesli masz uwagi,napisz';
+
+  onSubmit() {
+    console.log(this.Imie_Nazwisko, this.title, this.ilosc);
+  }
 }
